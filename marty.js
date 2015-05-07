532d24e2dac0720d1a1726e83ea4ddcb99d9251c
James Hollingworth <jamiehollingworth@gmail.com>
2015-05-07 06:57:35 +0100

    Use marty-lib

--- marty.js
+++ marty.js
@@ -1,13 +1,23 @@
-var Marty = require('marty-core').Marty;
-var marty = new Marty('0.10.0-alpha', require('react-native'));
+var Marty = require('marty-lib/modules/core/marty');
+var marty = new Marty('0.10.0-beta', react());
 
-marty.use(require('marty-core'));
-marty.use(require('marty-constants'));
-marty.use(require('marty-store'));
-marty.use(require('marty-action-creators'));
-marty.use(require('marty-queries'));
-marty.use(require('marty-state-mixin'));
-marty.use(require('marty-container'));
-marty.use(require('marty-http-state-source'));
+marty.use(require('marty-lib/modules/core'));
+marty.use(require('marty-lib/modules/constants'));
+marty.use(require('marty-lib/modules/application'));
+marty.use(require('marty-lib/modules/store'));
+marty.use(require('marty-lib/modules/action-creators'));
+marty.use(require('marty-lib/modules/queries'));
+marty.use(require('marty-lib/modules/state-mixin'));
+marty.use(require('marty-lib/modules/container'));
 
 module.exports = marty;
+
+// Due to [NPM peer dependency issues](https://github.com/npm/npm/issues/5875)
+// we need to try resolving react from the parent if its not present locally
+function react() {
+  try {
+    return require('react-native');
+  } catch (e) {
+    return module.parent.require('react-native');
+  }
+}
